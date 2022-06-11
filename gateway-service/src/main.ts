89c1b081aa4578036029d3d6fa8dd5a08b8a412f
Kwandes <bz.jasiek@gmail.com>
2022-06-11 05:40:25 +0200

    Add SwaggerUi API Docs to gateway at `/docs`

--- gateway-service/src/main.ts
+++ gateway-service/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { RpcExceptionFilter } from './filters/rpc-exception.filter';
 
@@ -20,6 +21,15 @@ async function bootstrap() {
       forbidUnknownValues: true,
     }),
   );
+
+  // Setup swagger docs
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs Microservices example')
+    .setVersion('0.123')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
